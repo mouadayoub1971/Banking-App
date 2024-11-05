ed223b6ca04a293a7f094794e64a959a224fa5fa
mouad ayoub <mouadayoubmouadayoub@gmail.com>
2024-11-05 15:21:56 +0100

    login page is ready

--- src/main/java/com/example/banking_app/Controllers/LoginController.java
+++ src/main/java/com/example/banking_app/Controllers/LoginController.java
@@ -1,5 +1,16 @@
 package com.example.banking_app.Controllers;
 
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
 public class LoginController {
 
+    public ChoiceBox acc_selector;
+    public Label payee_adress;
+    public TextField payee_adress_fld;
+    public TextField password_lbl;
+    public Button login_btn;
+    public Label error_lbl;
 }
